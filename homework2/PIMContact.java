c06497f74318ec169750e8f2fd677ed4bb4bbb65
YetToCome <wjw15129@gmail.com>
2014-03-18 12:38:33 +0800

    add contact.java

--- homework2/PIMContact.java
+++ homework2/PIMContact.java
@@ -9,6 +9,10 @@ class PIMContact extends PIMEntity {
     String first_name, last_name, email_address;
     public void fromString(String s) {
         String[] arr_line = s.split("\\+");
+        first_name = arr_line[0];
+        last_name = arr_line[1];
+        email_address = arr_line[2];
+        setPriority(arr_line[3]);
     }
     public String toString() {
         return getPriority() + " " + first_name + " " + last_name + " " + email_address;
